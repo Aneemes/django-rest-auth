77da0420e30f0ddda11edaf55911d7209c5f05ec
aneemes <aneemes1@gmail.com>
2024-01-18 19:46:29 +0545

    dj-rest-auth-w-socials-setup

---
+++ README.md
@@ -0,0 +1,19 @@
+#Django REST Framework User Authentication
+***
+###This Django project provides user authentication functionalities using popular packages such as django-allauth, dj-rest-auth, and djangorestframework. It includes features for login, registration, password reset, password change, email verification, and social media logins (Google, Facebook, GitHub).
+***
+###Features
+####Login & Registration:
+Users can register for an account and log in securely.
+---
+####Password Management:
+Password reset functionality allows users to recover their account.
+Users can change their password for enhanced security.
+---
+####Email Verification:
+Verify user email addresses to ensure account authenticity.
+---
+####Social Media Logins:
+Google Login: Users can sign in using their Google accounts.
+Facebook Login: Users can log in using their Facebook credentials.
+GitHub Login: Integration with GitHub for secure authentication.
---
+++ authentication/views.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
+from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+
+def email_confirm_redirect(request, key):
+    return HttpResponseRedirect(
+        f"{settings.EMAIL_CONFIRM_REDIRECT_BASE_URL}{key}/"
+    )
+
+
+def password_reset_confirm_redirect(request, uidb64, token):
+    return HttpResponseRedirect(
+        f"{settings.PASSWORD_RESET_CONFIRM_REDIRECT_BASE_URL}{uidb64}/{token}/"
+    )
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://localhost:8000/"
+    client_class = OAuth2Client
+
+class FacebookLogin(SocialLoginView):
+    adapter_class = FacebookOAuth2Adapter
+    
+class GithubLogin(SocialLoginView):
+    adapter_class = GitHubOAuth2Adapter
+    callback_url = "http://127.0.0.1:8000/accounts/github/login/callback/"
+    client_class = OAuth2Client
